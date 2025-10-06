d66e1d19ac6f2b75c48b2d292c8db3204eb3358e
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-10-06 00:33:58 -0700

    Add starting "CLI" environment
    Following requirements in:
    https://roadmap.sh/projects/task-tracker

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from task import Task
 
-new_task = Task()
-print(new_task)
+# CLI-like environment to accept commands
+
+COMMANDS = ("add", "update", "delete")
+COLORS = {
+    "PURPLE": "\033[35m",
+    "RESET": "\033[0m",
+}
+
+def runCLI():
+    print(
+        f"Welcome to your to-do list app! Please enter a command ({', '.join(command for command in COMMANDS)}):"
+    )
+
+    while True:
+        print(f"{COLORS['PURPLE']}task-cli >{COLORS['RESET']}", end="", flush=True)
+        userInput = input().split()
+        print(userInput)
+
+
+if __name__ == "__main__":
+    runCLI()
