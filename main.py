79e23df2752aaaa60850f7f71294ae9a44b45839
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-10-12 00:56:13 -0700

    Move CLI logic to cli.py

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-from task import Task
-
-# CLI-like environment to accept commands
-
-COMMANDS = ("add", "update", "delete")
-COLORS = {
-    "PURPLE": "\033[35m",
-    "RESET": "\033[0m",
-}
-
-def runCLI():
-    print(
-        f"Welcome to your to-do list app! Please enter a command ({', '.join(command for command in COMMANDS)}):"
-    )
-
-    while True:
-        print(f"{COLORS['PURPLE']}task-cli >{COLORS['RESET']}", end="", flush=True)
-        userInput = input().split()
-        print(userInput)
-
+from cli import runCLI
 
 if __name__ == "__main__":
     runCLI()
