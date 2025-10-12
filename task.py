21c3d4fcbebc27a6463c22e445a94565a41041e3
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-10-12 16:52:02 -0700

    Simplify Task class

--- task.py
+++ task.py
@@ -1,27 +1,19 @@
+from datetime import datetime
 class Task:
-    def __init__(self):
-        self.description = self.getDescription()
-        self.completed = self.getCompleted()
+    STATUSES = ("todo", "in-progress", "done")
 
-    def getDescription(self) -> str:
-        task_desc = ""
-
-        while not task_desc.strip():
-            task_desc = input("What is the task? ")
-            if not task_desc:
-                print("Error: Task cannot be empty. Try again.")
-
-        return task_desc
-
-    def getCompleted(self) -> str:
-        completion_status = ""
-
-        while not completion_status.strip():
-            completion_status = input("Has this task been completed? ")
-            if not completion_status:
-                print("Error: Completion status cannot be empty. Try again.")
-
-        return completion_status
+    def __init__(
+        self,
+        id,
+        description,
+    ):
+        self.id = id
+        self.description = description
+        self.status = self.STATUSES[0]  # new tasks are automatically 'todo'
+        self.createdAt = datetime.now()
+        self.updatedAt = (
+            self.createdAt  # createdAt & updatedAt are the same when a new task is created
+        )
 
     def __str__(self):
-        return f"Task: {self.description}\nCompletion status: {self.completed}"
+        return f"ID: {self.id}\nDescription: {self.description}\nStatus: {self.status}\nCreated At (MM/DD/YYYY): {self.createdAt.strftime('%m/%d/%Y')}\nUpdated At (MM/DD/YYYY): {self.updatedAt.strftime('%m/%d/%Y')}"
