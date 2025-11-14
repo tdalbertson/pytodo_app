f2a41be17488522201a938f3ba6f97b74f514f2d
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-11-13 17:59:25 -0800

    Add to_dict method

--- task.py
+++ task.py
@@ -6,13 +6,16 @@ class Task:
         self,
         id,
         description,
+        status=None,
+        created_at=None,
+        updated_at=None
     ):
         self.id = id
         self.description = description
         self.status = self.STATUSES[0]  # new tasks are automatically 'todo'
-        self.createdAt = datetime.now()
-        self.updatedAt = (
-            self.createdAt  # createdAt & updatedAt are the same when a new task is created
+        self.created_at = datetime.now()
+        self.updated_at = (
+            self.created_at  # created_at & updated_at are the same when a new task is created
         )
 
     def __str__(self):
@@ -20,6 +23,24 @@ class Task:
 ID: {self.id}
 \tDescription: {self.description}
 \tStatus: {self.status}
-\tCreated At (MM/DD/YYYY): {self.createdAt.strftime("%m/%d/%Y, %H:%M")}
-\tUpdated At (MM/DD/YYYY): {self.updatedAt.strftime("%m/%d/%Y, %H:%M")}
+\tCreated At (MM/DD/YYYY): {self.created_at.strftime("%m/%d/%Y, %H:%M")}
+\tUpdated At (MM/DD/YYYY): {self.updated_at.strftime("%m/%d/%Y, %H:%M")}
 """
+    
+    def to_dict(self) -> dict:
+        """
+        Converts a Task object into a dictionary format for JSON serialization
+
+        Args:
+            None
+        
+        Returns:
+            dict: A dictionary representation of the Task with all attributes
+        """
+        return {
+            "id": self.id,
+            "description": self.description,
+            "status": self.status,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat()
+        }
