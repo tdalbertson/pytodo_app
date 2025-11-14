d03128fcad61b1c9413c55fb7429651d4efca14a
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-11-13 18:02:40 -0800

    Add todo.write_tasks_to_file() to save added tasks after exiting CLI

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from todo_list import ToDoList
 if __name__ == "__main__":
     todo = ToDoList("tasks.json")
     run_CLI(todo)
+
+    # Write existing tasks once exiting from the tasks CLI
+    todo.write_tasks_to_file()
