8fc4553b485b885a7b2e3fe6b85c830a8b271cda
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-12-03 00:21:03 -0800

    Add update_task methods and TODO to combine them

--- todo_list.py
+++ todo_list.py
@@ -1,6 +1,8 @@
 import json
 import os
+from datetime import datetime
 from task import Task
+
 
 class ToDoList:
     def __init__(self, filename="tasks.json"):
@@ -13,8 +15,11 @@ class ToDoList:
         Return a list of tasks.
 
         If a tasks file exists, it will be loaded and the list will be
-        populated with existing tasks. 
+        populated with existing tasks.
         Otherwise, a new file will be created and an empty list will be returned.
+
+        Args:
+            None
 
         Returns:
             list[Task]: A list of Task objects representing the current tasks.
@@ -55,12 +60,15 @@ class ToDoList:
 
             json.dump(tasks_data, f, indent=4)
 
-    #TODO
+    # TODO
     def list_tasks(self, status=None) -> list[str]:
         """
         Return a list of string representations of tasks.
         If a status is provided, tasks with associated status will be returned.
         Otherwise, all tasks in the list will be returned.
+
+        Args:
+            None
 
         Returns:
             list[str]: A list containing string representations of tasks.
@@ -74,6 +82,9 @@ class ToDoList:
     def get_next_ID(self) -> int:
         """
         Return an integer that represents the ID of next available ID
+
+        Args:
+            None
 
         Returns:
             int: 1 if no tasks exist indicating the start of tasks
@@ -90,6 +101,7 @@ class ToDoList:
 
         Args:
             task_description (str): The description of the task to add.
+
         Returns:
             None
         """
@@ -103,6 +115,7 @@ class ToDoList:
 
         Args:
             id (int): The id of the task to remove.
+
         Returns:
             None
         """
@@ -130,11 +143,11 @@ class ToDoList:
             if task.id == target_id:
                 return i
         return None
-
+    #TODO: Combine update methods as code repeats
     def update_task_status(self, id: int, status: str) -> None:
         """
         Update a task's status based on specified status.
-        If an ID cannot be found, the user is shown an failure message.
+        If an ID cannot be found, the user is shown a failure message.
         Otherwise, the associated task's status is updated and the user is shown a success message.
 
         Args:
@@ -150,6 +163,30 @@ class ToDoList:
                 f"Task with ID {id} could not be found. Please try again with another ID."
             )
         else:
-            updated_task = self.tasks[task_index]
-            updated_task.status = status
-            print(f"Updated task {updated_task.description} to {status}")
+            task_soon_to_be_updated = self.tasks[task_index]
+            task_soon_to_be_updated.status = status
+            task_soon_to_be_updated.updated_at = datetime.now()
+            print(f"Updated task {task_soon_to_be_updated.description} to {status}")
+
+    def update_task_description(self, id: int, new_description: str):
+        """
+        Find's the specified task via its ID and updates its description with the
+        new_description
+
+        Args:
+            id (int): The ID of the task whose description to update.
+            new_description (str): The new task description to update the task.
+
+        Returns:
+            None
+        """
+        task_index = self.get_task_by_id(id)
+        if task_index is None:
+            print(
+                f"Task with ID {id} could not be found. Please try again with another ID."
+            )
+        else:
+            task_to_be_updated = self.tasks[task_index]
+            task_to_be_updated.description = new_description
+            task_to_be_updated.updated_at = datetime.now()
+            print(f"Updated task {task_to_be_updated.description} to {new_description}")
