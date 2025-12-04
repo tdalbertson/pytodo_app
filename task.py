5190061c504f56b83d8fe9ea4c64c5be6270ad4f
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-12-03 23:28:15 -0800

    Clean up spacing

--- task.py
+++ task.py
@@ -1,28 +1,21 @@
 from datetime import datetime
+
+
 class Task:
     STATUSES = ("todo", "in-progress", "done")
 
-    def __init__(
-        self,
-        id,
-        description,
-        status=None,
-        created_at=None,
-        updated_at=None
-    ):
+    def __init__(self, id, description, status=None, created_at=None, updated_at=None):
         self.id = id
         self.description = description
         # If status/timestamps were provided (e.g., when loading from existing tasks file), keep them.
         self.status = status or self.STATUSES[0]
         self.created_at = (
-            datetime.fromisoformat(created_at)
-            if created_at
-            else datetime.now()
+            datetime.fromisoformat(created_at) if created_at else datetime.now()
         )
         self.updated_at = (
             datetime.fromisoformat(updated_at)
             if updated_at
-            else self.created_at
+            else self.created_at  # New tasks have the same created_at date
         )
 
     def __str__(self):
@@ -33,14 +26,14 @@ ID: {self.id}
 \tCreated At (MM/DD/YYYY): {self.created_at.strftime("%m/%d/%Y, %H:%M")}
 \tUpdated At (MM/DD/YYYY): {self.updated_at.strftime("%m/%d/%Y, %H:%M")}
 """
-    
+
     def to_dict(self) -> dict:
         """
         Converts a Task object into a dictionary format for JSON serialization
 
         Args:
             None
-        
+
         Returns:
             dict: A dictionary representation of the Task with all attributes
         """
@@ -49,5 +42,5 @@ ID: {self.id}
             "description": self.description,
             "status": self.status,
             "created_at": self.created_at.isoformat(),
-            "updated_at": self.updated_at.isoformat()
+            "updated_at": self.updated_at.isoformat(),
         }
